c2af74dab62b36156a58e58c675418dfa6420fdf
Bradyn Kleinheksel <bkfactor1@outlook.com>
2023-05-15 23:52:35 -0500

    Updates to notes.

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -13,14 +13,15 @@ def recursive_binary_search(list, target):
             if list[midpoint] < target:
                 return recursive_binary_search(list[midpoint+1:], target) # This return statement and the one below it end up creating entirely new list based off
             else:                                                         # The original one that was sent to be processed, which are sent through the same function
-                return recursive_binary_search(list[:midpoint], target)   # recursivly. 
-            
-def verify(result):                                                       # : Is used to slice the list, and if you dont have any value on one side of the : it will
-    print("Target found: ", result)                                       # automatically auto fill to the start or end of the list. 
-                        
-numbers = [0,1,2,3,4,5,6,7,8]
-result = recursive_binary_search(numbers, 12)
-verify(result)
-
+                return recursive_binary_search(list[:midpoint], target)   # recursivly. The whole idea of recurssion is that the algorithm is called with a sequence and,
+                                                                          # The aglorithm keeps sending the same sequence through the same algorithm with a return statement
+                                                                          # until the target is found or a exception is formed. The number of times a recursive function calls itself is called recursive depth. 
+def verify(result):                                                       
+    print("Target found: ", result)                                       
+                                                                          # : Is used to slice the list, and if you dont have any value on one side of the : it will automatically auto fill to the start or end of the list. 
+numbers = [0,1,2,3,4,5,6,7,8]                                             #
+result = recursive_binary_search(numbers, 12)                             # Some langauges are better then others for recursion. Python for example may give a
+verify(result)                                                            # maximum "recursion depth exceeded" error if the recursive function calls istelf too many times.
+                                                                          
 result = recursive_binary_search(numbers, 6)
 verify(result)
